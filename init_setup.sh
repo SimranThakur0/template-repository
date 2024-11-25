7fcbcbddf9e5156e1c8a1edd83dd7ed9f39131dd
Simran thakur <shivangithakur7300@gmail.com>
2024-11-26 01:50:12 +0530

    updated setup file

--- init_setup.sh
+++ init_setup.sh
@@ -3,6 +3,6 @@ echo [$(date)]: "Creating env with python 3.8 version"
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activating the env"
 source activate ./env
-echo [$(date)]: "installing required packages"
-pip install -r requirements_dev.txt
-echo [$(date)]: "END"
+# echo [$(date)]: "installing required packages"
+# pip install -r requirements_dev.txt
+# echo [$(date)]: "END"
